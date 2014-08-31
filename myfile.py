4b13b3034186cff7e628fc7c432aeb1a2a7db8a7
qtchristian <the_cheese30@yahoo.com>
2014-08-31 17:39:48 -0500

    red circle follows mouse

--- myfile.py
+++ myfile.py
@@ -12,6 +12,7 @@ blue = (0,0,255)
 yellow = (255,255,0)
 cyan = (0,255,255)
 purple = (255,0,255)
+mousepos = (0,0)
 
 setDisplay = pygame.display.set_mode((400,300))
 pygame.display.set_caption('Hello')
@@ -19,15 +20,19 @@ pygame.display.set_caption('Hello')
 setDisplay.fill(purple)
 
 
-pygame.draw.circle(setDisplay,red, (50,50),20,10)\
-
-
+def drawcircle(color,posistion): 
+	pygame.draw.circle(setDisplay,color,posistion,50,0)
 
 while True:
 	for event in pygame.event.get():
-	  print event
+	 
 	  if event.type == QUIT:
 		pygame.quit()
 		sys.exit()
-
+        
+ 
+        mousepos = pygame.mouse.get_pos()
+        setDisplay.fill(black)
+		
+        drawcircle(red, mousepos)
 	pygame.display.update()
